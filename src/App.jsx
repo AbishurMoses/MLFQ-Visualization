bc356b30f307685a331cabbd20b56dd3f140fc59
Abishur Moses <abishur@gmail.com>
2024-04-29 14:38:22 -0400

    Completed custom job creation.

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,67 @@ import './App.css'
 import React from 'react'
 import { useState, useEffect } from 'react'
 import QueueGraph from './components/QueueGraph'
+import Job from './components/Job'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Slider from '@mui/material/Slider';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
+/*	Jobs need ids
+		- Will need to find a way to create both seeded and custom job Ids
+	Names should not be the same
+		- Should the option be restricted or left to the user
+	Job deletion
+		- Seeded jobs might need to be deleted
+			- However, this means that the user generated jobs can't be removed once added
+*/
 function App() {
 	const [massJobs, setMassJobs] = useState(false)
+	const [seedBtn, setSeedBtn] = useState(false)
+	const [jobName, setJobName] = useState("")
+	const [jobLength, setJobLength] = useState(0)
+	const [jobInteractivity, setJobInteractivity] = useState(0)
+	// Pre-determined Jobs
 	var PD = []
+	// Jobs created by the user
+	var custom_jobs = []
 
 	function getRandomInt(max) {
 		return Math.floor(Math.random() * max);
 	}
 
+	// Custom Job
+	const addJob = () => {
+		if (jobName == "") {
+			alert("Add a name dommy")
+		} else {
+			custom_jobs.push({
+				name: jobName,
+				length: jobLength,
+				jobInteractivity: jobInteractivity
+			})
+			console.log(custom_jobs)
+		}
+	}
+
+	// Seeded jobs
 	const addJobs = (numOfJobs) => {
 		var i = 0;
 		while (i < numOfJobs) {
 			PD.push({
-				id: i,
+				name: "Job " + (i + 1).toString(),
 				length: getRandomInt(25),
 				interactivity: getRandomInt(3),
 			})
 			i++
 		}
+	}
+
+	const resetInput = () => {
+		setJobName("")
+		setJobLength(0)
+		setJobInteractivity(0)
 	}
 
 	useEffect(() => {
@@ -32,15 +74,34 @@ function App() {
 	}
 
 	const seedJobs = () => {
-		addJobs(3)
-		console.log(PD)
+		if (seedBtn == false) {
+			addJobs(3)
+			console.log(PD)
 
-		fnMassJobs(true)
+			setSeedBtn(true)
+			fnMassJobs(true)
+		} else {
+			alert("Max jobs seeded")
+		}
 	}
 
 	const clearJobs = async () => {
 		PD = []
+
+		setSeedBtn(false)
 		fnMassJobs(false)
+	}
+
+	const handleJob = (event) => {
+		if (event.target.id == "job-name") {
+			setJobName(event.target.value)
+		}
+		if (event.target.name == "job-length") {
+			setJobLength(event.target.value)
+		}
+		if (event.target.name == "job-interactivity") {
+			setJobInteractivity(event.target.value)
+		}
 	}
 
 	return (
@@ -58,18 +119,36 @@ function App() {
 								)}
 							</div>
 							<div id="custom-job">
-								<h3>Custom Job</h3>
+								<Box
+									component="form"
+									id="job-input"
+									noValidate
+									autoComplete="off"
+								>
+									<TextField id="job-name" value={jobName} onChange={handleJob} label="Name" variant="outlined" />
+									<div className="input-container">
+										<p>Length</p>
+										<Slider name="job-length" className="sliders" value={jobLength} onChange={handleJob} defaultValue={25} valueLabelDisplay="auto" max={50} />
+									</div>
+									<div className="input-container">
+										<p>Inter</p>
+										<Slider name="job-interactivity" className="sliders" value={jobInteractivity} onChange={handleJob} defaultValue={1} valueLabelDisplay="auto" max={2} />
+									</div>
+								</Box>
+								<Stack id="job-btn-container">
+									<Button className="custom-btns" variant="contained" onClick={addJob}>Add</Button>
+									<Button className="custom-btns" variant="contained" onClick={resetInput}>Reset</Button>
+								</Stack>
 							</div>
 						</div>
 						<div id="job-buttons">
-							<div className="btns" id="seed-btn" onClick={seedJobs}>Seed Jobs</div>
-							<div className="btns" id="clear-btn" onClick={clearJobs}>Clear</div>
+							<Button className="noncustom-btns" variant="contained" style={{ cursor: seedBtn ? "not-allowed" : "default" }} onClick={seedJobs}>Seed</Button>
+							<Button className="noncustom-btns" variant="contained" onClick={clearJobs}>Clear</Button>
 						</div>
 					</div>
 				</div>
 				<div id="MLFQ-container">
-					<QueueGraph />
-					<p>Controls</p>
+					{/* <QueueGraph /> */}
 					{/* <div id="controls">
 						<div className="cols" id="col1">
 							<p>Time Allotment per Queue</p>
@@ -109,7 +188,7 @@ function App() {
 					</div> */}
 				</div>
 			</div>
-		</div>
+		</div >
 	)
 }
 
